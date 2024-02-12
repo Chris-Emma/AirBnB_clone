ecb775ca90b67cf1f3d7f6cb1b72e3236d0cdeae
Chris-Emma <chrisemmaofficial@gmail.com>
2024-02-12 07:08:11 -0800

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,22 @@ import models
 class BaseModel:
     """ Defines all common attributes/methods for other classes """
 
-    def __init__(self):
-        """Initializes a new instance of the Base Model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initializes a new Instance of the Base Mode
+        - *args: Will not be used
+        - **kwargs: a dict containing key-value arguments
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    if not isinstance(value, datetime):
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self) -> str:
         """Returns the string representation of an instance"""
